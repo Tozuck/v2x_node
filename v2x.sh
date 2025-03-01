3f55aed57fd57c57c607e3ea0ab350d657d81137
Tozuck <122993981+Tozuck@users.noreply.github.com>
2025-03-02 00:11:22 +0330

    Update v2x.sh

--- v2x.sh
+++ v2x.sh
@@ -112,8 +112,8 @@ docker compose up -d || echo_error "Failed to start Marzban Node service."
 
 # Enable and reload UFW
 echo_info "Finalizing UFW setup..."
-ufw -force enable
 ufw enable
+ufw reload
 
 
 # Clear shell history for security
